8a4b2047da3c7e9473ed0c7ea25a075858a905e1
Jdpa357 <jdpa352@hotmail.es>
2020-05-08 02:46:23 +0000

    Adding tasks to project

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module has one function: number_of_subscribers(subreddit).
+"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries a subreddit and returns the number of subscribers.
+    """
+    # URL and headers to make API requests from
+    url = 'https://reddit.com/r'
+
+    # Subscriber count information
+    subscriber_count = requests.get('{}/{}/about.json?subscribers'
+                                    .format(url, subreddit),
+                                    headers={'User-Agent': 'Mozilla/5.0'}) \
+        .json().get('data').get('subscribers')
+
+    if not subscriber_count:
+        subscriber_count = 0
+
+    return subscriber_count
