f95c81605e6bb7ad16d24bc6d22e89e55aa3a645
Jdpa357 <jdpa352@hotmail.es>
2020-05-21 14:08:27 +0000

    Adding puppet file to project

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Puppet file that fixes wrong 'phpp' extensions to 'php' in the WordPress
+# file 'wp-settings.php'.
+
+exec { 'fix':
+    command  => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
+    provider => shell,
+}
